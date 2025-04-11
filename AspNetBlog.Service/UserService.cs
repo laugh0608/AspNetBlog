a5f2d178cbd04f6c5310495366852baedf4ae96b
laugh0608 <laugh0608@foxmail.com>
2025-04-11 14:25:53 +0800

    添加Service服务层

---
+++ AspNetBlog.Service/UserService.cs
@@ -0,0 +1,16 @@
+﻿using AspNetBlog.IService;
+using AspNetBlog.Model;
+using AspNetBlog.Repository;
+
+namespace AspNetBlog.Service;
+
+// 服务层
+public class UserService : IUserService
+{
+    public async Task<List<UserViewObject>> Query()
+    {
+        var userRepo = new UserRepository();
+        var users = await userRepo.Query();
+        return users.Select(d => new UserViewObject() { UserName = d.Name }).ToList();
+    }
+}
